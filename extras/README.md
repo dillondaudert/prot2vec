75f525aa7b58f9bb44ebbf92fce31d403fda8778
Dillon Daudert <dillongdaudert@gmail.com>
2018-01-09 17:04:39 -0500

    Add MakeTFRecords and make_tfrecords.py

---
+++ extras/README.md
@@ -0,0 +1,12 @@
+# extras
+...
+## Dataset Preparation
+
+The original dataset was prepared for Zhou & Troyanskaya (see references), 
+described in the *cpdb_readme.txt* file. This dataset consisted of proteins
+taken from the PDB up to 700 residues in length, where proteins shorter than
+this were padded with 'NoSeq' tokens. 
+
+Input to seq2seq models allows for variable-length sequences, so the original
+dataset was modified and saved as a TFRecord format. This format makes it easy
+to read into TensorFlow using the tf.data API.
---
+++ extras/make_tfrecords.py
@@ -0,0 +1,70 @@
+# utility functions
+from pathlib import Path
+import numpy as np
+import tensorflow as tf
+
+HOME = str(Path.home())
+
+def _floats_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+def cpdb_to_tfrecord():
+    """
+    Convert the numpy array format for cpdb files to TFRecord format
+    Saves training, validation, and test files
+    """
+
+    data = np.load(HOME+"/data/cpdb/cpdb_6133.npy.gz").reshape(6133, 700, 57)
+
+    seqs = np.concatenate([data[:, :, 0:22].copy(), data[:, :, 35:56].copy()], axis=2).reshape(6133, -1)
+    labels = data[:, :, 22:31].copy().reshape(6133, 700, -1)
+
+    num_features = 43
+    num_labels = 9
+
+    # Count the protein sequence lengths for all samples
+    def get_length(seq_labels):
+        assert seq_labels.shape == (700, 9)
+        noseq = np.array([[0., 0., 0., 0., 0., 0., 0., 0., 1.]])
+        return np.logical_not(np.all(np.equal(seq_labels, noseq), axis=1)).sum()
+
+    seq_lengths = [get_length(labels[l, :, :]) for l in range(6133)]
+
+    # Flatten labels
+    labels = labels.reshape(6133, -1)
+
+    train_examples = range(5600)
+    valid_examples = range(5600,5877)
+    test_examples = range(5877,6133)
+
+    trainfile = HOME+"./data/cpdb/cpdb_6133_train.tfrecords"
+    validfile = HOME+"./data/cpdb/cpdb_6133_valid.tfrecords"
+    testfile = HOME+"/data/cpdb/cpdb_6133_test.tfrecords"
+    print("Writing ", trainfile)
+    trainwriter = tf.python_io.TFRecordWriter(trainfile)
+
+    for index in train_examples:
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'seq_data': _floats_feature(seqs[index, 0:num_features*seq_lengths[index]]),
+            'label_data': _floats_feature(labels[index, 0:num_labels*seq_lengths[index]])}))
+        trainwriter.write(example.SerializeToString())
+    trainwriter.close()
+
+    print("Writing ", validfile)
+    validwriter = tf.python_io.TFRecordWriter(validfile)
+    for index in valid_examples:
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'seq_data': _floats_feature(seqs[index, 0:num_features*seq_lengths[index]]),
+            'label_data': _floats_feature(labels[index, 0:num_labels*seq_lengths[index]])}))
+        validwriter.write(example.SerializeToString())
+    validwriter.close()
+
+
+    print("Writing ", testfile)
+    testwriter = tf.python_io.TFRecordWriter(testfile)
+    for index in test_examples:
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'seq_data': _floats_feature(seqs[index, 0:num_features*seq_lengths[index]]),
+            'label_data': _floats_feature(labels[index, 0:num_labels*seq_lengths[index]])}))
+        testwriter.write(example.SerializeToString())
+    testwriter.close()
