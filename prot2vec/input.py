3b62ff39456b6054f782aadb637cc30856111ce2
Dillon Daudert <dillongdaudert@gmail.com>
2018-01-11 22:53:58 -0500

    cpdb_6133_input_fn now properly returns padded batches of sequences

--- prot2vec/input.py
+++ prot2vec/input.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 HOME = str(Path.home())
 
-def cpdb_6133_input_fn(batch_size, shuffle, num_epochs, mode, window_size):
+def cpdb_6133_input_fn(batch_size, shuffle, num_epochs, mode):
     """
     An Estimator input function using the tf.data API
     """
@@ -37,27 +37,26 @@ def cpdb_6133_input_fn(batch_size, shuffle, num_epochs, mode, window_size):
         seq_len = parsed["seq_len"]
         seq = tf.sparse_tensor_to_dense(parsed["seq_data"])
         label = tf.sparse_tensor_to_dense(parsed["label_data"])
+        seq = tf.reshape(seq, [-1, 43])
+        label = tf.reshape(label, [-1, 9])
 
-        return {"seq_data": seq}, label, seq_len
+        return seq, label
 
     # apply parser transformation to parse out individual samples
     dataset = dataset.map(parser)
 
-    dataset = dataset.padded_batch(
-            batch_size,
-            padded_shape=((tf.TensorShape([None]),
-                           tf.TensorShape([])),
-                          (tf.TensorShape([None]),
-                           tf.TensorShape([]))),
-            padding_values=((
-                )))
-
     if shuffle:
         dataset = dataset.shuffle(buffer_size=64)
-    dataset = dataset.batch(batch_size)
     dataset = dataset.repeat(num_epochs)
 
-    # 3) make an iterator object that provides access to one element at a time
+    dataset = dataset.padded_batch(
+            batch_size,
+            padded_shapes=(tf.TensorShape([None, 43]),
+                           tf.TensorShape([None, 9]))
+            )
+
+
+    # 3) make an iterator object
     iterator = dataset.make_one_shot_iterator()
 
     # ((source, source_lengths), (target, target_lengths)) = iterator.get_next()
