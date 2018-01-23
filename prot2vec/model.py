7f61d2b8a3d62175774f6d8babebe6cd1219182a
Dillon Daudert <dillongdaudert@gmail.com>
2018-01-23 18:31:02 -0500

    Change Dense layer to TimeDistributed Dense for increased clarity

--- prot2vec/model.py
+++ prot2vec/model.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Input, LSTMCell, RNN, Dense, Masking
+from keras.layers import Input, LSTMCell, RNN, Dense, Masking, TimeDistributed
 
 class EncDecModel():
     '''LSTM encoder-decoder network as multiple Keras models.'''
@@ -10,17 +10,17 @@ class EncDecModel():
     models = {}
 
     def __init__(self,
-                 num_features,
                  num_targets,
                  src_in,
                  tgt_in,
                  tgt_out,
-                 filename=None):
-        self.num_features = num_features
+                 filename=None
+                 teacher_forcing=True):
         self.num_targets = num_targets
         self.src_in = src_in
         self.tgt_in = tgt_in
         self.tgt_out = tgt_out
+        self.teacher_forcing = teacher_forcing
         self._create_models()
         if filename != None:
             # load the weights into the models
@@ -36,9 +36,7 @@ class EncDecModel():
         encoder_mask = Masking(mask_value=0.)(encoder_in)
 
         encoder_cells = [
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='enc_lstm_1', unit_forget_bias=True),
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='enc_lstm_2', unit_forget_bias=True),
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='enc_lstm_3', unit_forget_bias=True),
+            LSTMCell(units=128, dropout=0.2, recurrent_dropout=0.2, name='enc_lstm_1', unit_forget_bias=True),
         ]
 
         encoder = RNN(encoder_cells, return_state=True, go_backwards=True, name='enc_rnn')
@@ -54,15 +52,13 @@ class EncDecModel():
         decoder_mask = Masking(mask_value=0.)(decoder_in)
 
         decoder_cells = [
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='dec_lstm_1', unit_forget_bias=True),
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='dec_lstm_2', unit_forget_bias=True),
-            LSTMCell(units=256, dropout=0.2, recurrent_dropout=0.2, name='dec_lstm_3', unit_forget_bias=True),
+            LSTMCell(units=128, dropout=0.2, recurrent_dropout=0.2, name='dec_lstm_1', unit_forget_bias=True),
         ]
         decoder_lstm = RNN(decoder_cells, return_sequences=True, return_state=True, name='dec_rnn')
 
         decoder_outputs = decoder_lstm(decoder_mask, initial_state=encoder_states)
 
-        decoder_dense = Dense(self.num_targets, activation='softmax', name='dec_dense')
+        decoder_dense = TimeDistributed(Dense(self.num_targets, activation='softmax', name='dec_dense'))
         decoder_out = decoder_dense(decoder_outputs[0])
 
         # Define model to turn 'enc_input_data' & 'dec_input_data' into 'dec_target_data'
