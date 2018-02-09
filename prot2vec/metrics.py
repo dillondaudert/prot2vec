7cf7a915e0f9c7b6004506e5be6252fdaebab0dc
Dillon Daudert <dillongdaudert@gmail.com>
2018-02-09 01:05:55 -0500

    Add weights streaming_confusion_matrix to for masking

--- prot2vec/metrics.py
+++ prot2vec/metrics.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-def streaming_confusion_matrix(labels, predictions, num_classes):
+def streaming_confusion_matrix(labels, predictions, num_classes, weights=None):
     """Calculates a confusion matrix.
 
     This creates local variables to track the confusion matrix statistics across
@@ -11,6 +11,7 @@ def streaming_confusion_matrix(labels, predictions, num_classes):
         labels: the ground truth labels, a Tensor of the same shape as predictions
         predictions: the prediction values, a Tensor of shape (?,)
         num_classes: the number of classes for this confusion matrix
+        weights: the weight of each prediction (default None)
     Returns:
         confusion: A k x k Tensor representing the confusion matrix, where
             the columns represent the predicted label and the rows represent the
@@ -22,6 +23,7 @@ def streaming_confusion_matrix(labels, predictions, num_classes):
     _confusion = tf.confusion_matrix(labels=labels,
                                      predictions=predictions,
                                      num_classes=num_classes,
+                                     weights=weights,
                                      name="_confusion")
 
     # accumulator for the confusion matrix
