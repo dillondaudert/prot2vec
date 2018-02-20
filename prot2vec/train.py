2ab04a28e2c3316751e7f8fed95ba31d4c53b556
Dillon Daudert <dillongdaudert@gmail.com>
2018-02-20 15:29:57 -0500

    Train loop no longer tracks epoch as the dataset repeats automatically without signal

--- prot2vec/train.py
+++ prot2vec/train.py
@@ -26,49 +26,49 @@ def train(hparams):
         local_initializer = tf.local_variables_initializer()
 
     # Summary writers
-    summary_writer = tf.summary.FileWriter(logdir, train_tuple.graph)
+    summary_writer = tf.summary.FileWriter(logdir, train_tuple.graph, max_queue=25, flush_secs=30)
 
     train_tuple.session.run([initializer])
 
-    # Train for num_epochs
-    for i in range(hparams.num_epochs):
-        train_tuple.session.run([train_tuple.iterator.initializer])
-        start_time = process_time()
+    start_time = process_time()
+    # initialize the training dataset
+    train_tuple.session.run([train_tuple.iterator.initializer])
+    # Train until the dataset throws an error (at the end of num_epochs)
+    while True:
         step_time = []
-        while True:
-            try:
-                curr_time = process_time()
-                _, train_loss, global_step, _, summary = train_tuple.model.train(train_tuple.session)
-                step_time.append(process_time() - curr_time)
+        try:
+            curr_time = process_time()
+            _, train_loss, global_step, _, summary = train_tuple.model.train(train_tuple.session)
+            step_time.append(process_time() - curr_time)
 
-                # write train summaries
-                if global_step == 1:
-                    summary_writer.add_summary(summary, global_step)
-                if global_step % 5 == 0:
-                    summary_writer.add_summary(summary, global_step)
-                    print("Step: %d, Training Loss: %f, Avg Step/Sec: %2.2f" % (global_step, train_loss, np.mean(step_time)))
+            # write train summaries
+            if global_step == 1:
+                summary_writer.add_summary(summary, global_step)
+            if global_step % 5 == 0:
+                summary_writer.add_summary(summary, global_step)
+                print("Step: %d, Training Loss: %f, Avg Step/Sec: %2.2f" % (global_step, train_loss, np.mean(step_time)))
 
-                if global_step % 20 == 0:
-                    checkpoint_path = train_tuple.model.saver.save(train_tuple.session,
-                                                                   ckptsdir,
-                                                                   global_step=global_step)
-                    eval_tuple.model.saver.restore(eval_tuple.session, checkpoint_path)
-                    eval_tuple.session.run([eval_tuple.iterator.initializer, local_initializer])
-                    while True:
-                        try:
-                            eval_loss, eval_acc, eval_summary, _ = eval_tuple.model.eval(eval_tuple.session)
-                            # summary_writer.add_summary(summary, global_step)
-                        except tf.errors.OutOfRangeError:
-                            print("Step: %d, Eval Loss: %f, Eval Accuracy: %f" % (global_step,
-                                                                                  eval_loss,
-                                                                                  eval_acc))
-                            summary_writer.add_summary(eval_summary, global_step)
-                            break
+            if global_step % 20 == 0:
+                # Do one evaluation
+                checkpoint_path = train_tuple.model.saver.save(train_tuple.session,
+                                                               ckptsdir,
+                                                               global_step=global_step)
+                eval_tuple.model.saver.restore(eval_tuple.session, checkpoint_path)
+                eval_tuple.session.run([eval_tuple.iterator.initializer, local_initializer])
+                while True:
+                    try:
+                        eval_loss, eval_acc, eval_summary, _ = eval_tuple.model.eval(eval_tuple.session)
+                        # summary_writer.add_summary(summary, global_step)
+                    except tf.errors.OutOfRangeError:
+                        print("Step: %d, Eval Loss: %f, Eval Accuracy: %f" % (global_step,
+                                                                              eval_loss,
+                                                                              eval_acc))
+                        summary_writer.add_summary(eval_summary, global_step)
+                        break
 
-            except tf.errors.OutOfRangeError:
-                print("- End of Epoch %d -" % (i+1))
-                break
-        summary_writer.flush()
+        except tf.errors.OutOfRangeError:
+            print("- End of Trainig -")
+            break
 
     # End of training
     summary_writer.close()
